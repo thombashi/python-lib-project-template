a5730e58b3d043ed053d2dac0184329a850b3009
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2017-11-12 12:12:01 +0900

    Initial commit

---
+++ README.rst
@@ -0,0 +1,10 @@
+Python package template
+============================================
+
+
+Run the tests
+============================================
+::
+
+    $ sudo pip install .[test]
+    $ python setup.py test
---
+++ test/test_template.py
@@ -0,0 +1,30 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import pytest
+
+
+def dummy_func(value):
+    return value
+
+
+class Test_Name(object):
+
+    @pytest.mark.parametrize(["value", "expected"], [
+        ["this test always fails", ""],
+    ])
+    def test_normal(self, value, expected):
+        assert dummy_func(value) == expected
+
+    @pytest.mark.parametrize(["value", "expected"], [
+        [None, ValueError],
+    ])
+    def test_exception(self, value, expected):
+        with pytest.raises(expected):
+            dummy_func(value)
