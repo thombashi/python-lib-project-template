f0dfaefdf352df6b900fc7c593a2df97d9ecd3f2
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-04-21 08:02:28 +0900

    Apply isort

--- test/test_template.py
+++ test/test_template.py
@@ -4,8 +4,7 @@
 .. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
 """
 
-from __future__ import print_function
-from __future__ import unicode_literals
+from __future__ import print_function, unicode_literals
 
 import pytest
 
