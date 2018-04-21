19a128efb13f185b269b4a9fa721570819c26563
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-04-21 20:30:09 +0900

    Add release command

--- setup.py
+++ setup.py
@@ -25,6 +25,15 @@ def need_pytest():
     return set(["pytest", "test", "ptr"]).intersection(sys.argv)
 
 
+def get_release_command_class():
+    try:
+        from releasecmd import ReleaseCommand
+    except ImportError:
+        return {}
+
+    return {"release": ReleaseCommand}
+
+
 with open(os.path.join(MODULE_NAME, "__version__.py")) as f:
     exec(f.read(), pkg_info)
 
@@ -42,6 +51,8 @@ with open(os.path.join(REQUIREMENT_DIR, "docs_requirements.txt")) as f:
 
 SETUPTOOLS_REQUIRES = ["setuptools>=38.3.0"]
 PYTEST_RUNNER_REQUIRES = ["pytest-runner"] if need_pytest() else []
+cmdclass = {}
+cmdclass.update(get_release_command_class())
 
 setuptools.setup(
     name=MODULE_NAME,
@@ -67,6 +78,7 @@ setuptools.setup(
     extras_require={
         "build": "wheel",
         "docs": DOCS_REQUIRES,
+        "release": "releasecmd>=0.0.7",
         "test": TESTS_REQUIRES,
     },
 
@@ -83,4 +95,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
-    ])
+    ],
+    cmdclass=cmdclass)
