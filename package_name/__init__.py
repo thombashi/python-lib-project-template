ed5a845896b42158c420e2238321bbbf9b2bbcab
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-08-12 18:45:13 +0900

    Remove codeauther directives

--- package_name/__init__.py
+++ package_name/__init__.py
@@ -1,7 +1,3 @@
 # encoding: utf-8
 
-"""
-.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
-"""
-
 from .__version__ import __author__, __copyright__, __email__, __license__, __version__
