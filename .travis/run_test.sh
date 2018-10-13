a396f30cc7a0687dc0264a0e0ef57e0ca861219c
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-10-13 10:25:01 +0900

    Add cov toxenv to Travis CI tests

--- .travis/run_test.sh
+++ .travis/run_test.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 pyver=$(python -c "from __future__ import print_function; import sys; print('{}{}'.format(*sys.version_info[0:2]))")
+echo $pyver
 
 tox
-
-if [ "$TRAVIS_OS_NAME" = "linux" ] && [ "$pyver" = "36" ]; then
-    python setup.py test --addopts "-v --cov XXXX --cov-report term-missing"
-fi
