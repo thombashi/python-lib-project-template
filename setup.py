dcd4386cc4c61cc141077e12569ef3390c8ca72b
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-01-01 08:06:01 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -47,8 +47,8 @@ with open(os.path.join(REQUIREMENT_DIR, "docs_requirements.txt")) as f:
 
 SETUPTOOLS_REQUIRES = ["setuptools>=38.3.0"]
 PYTEST_RUNNER_REQUIRES = ["pytest-runner"] if need_pytest() else []
-cmdclass = {}
-cmdclass.update(get_release_command_class())
+
+cmdclass = get_release_command_class()
 
 setuptools.setup(
     name=MODULE_NAME,
