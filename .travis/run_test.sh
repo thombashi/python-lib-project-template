58581e158417a63ee7a776e69d956a0cf6eda3cc
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-04 11:11:38 +0900

    Remoe a script file that no longer used

--- .travis/run_test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-pyver=$(python -c "from __future__ import print_function; import sys; print('{}{}'.format(*sys.version_info[0:2]))")
-echo $pyver
-
-if [ "$TOXENV" != "cov" ] ; then
-    tox
-fi
