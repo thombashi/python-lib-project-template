448cc1d79ec28c112e6f4f553fe69bcc074ac255
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-04 11:04:50 +0900

    Modify test_template

--- test/test_template.py
+++ test/test_template.py
@@ -5,17 +5,16 @@ def dummy_func(value):
     return value
 
 
-class Test_Name(object):
+def failed_func(value):
+    raise ValueError("always failed")
 
-    @pytest.mark.parametrize(["value", "expected"], [
-        ["this test always fails", ""],
-    ])
+
+class Test_Name:
+    @pytest.mark.parametrize(["value", "expected"], [["dummy test", "dummy test"],])
     def test_normal(self, value, expected):
         assert dummy_func(value) == expected
 
-    @pytest.mark.parametrize(["value", "expected"], [
-        [None, ValueError],
-    ])
+    @pytest.mark.parametrize(["value", "expected"], [[None, ValueError],])
     def test_exception(self, value, expected):
         with pytest.raises(expected):
-            dummy_func(value)
+            failed_func(value)
