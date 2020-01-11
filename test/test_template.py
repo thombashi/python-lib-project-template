d4a9959f058804662dc39cf3f7a490d31e02a749
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-11 17:07:39 +0900

    Modify dummy function definitions

--- test/test_template.py
+++ test/test_template.py
@@ -1,18 +1,12 @@
 import pytest
 
-
-def dummy_func(value: str) -> str:
-    return value
-
-
-def failed_func(value: str) -> None:
-    raise ValueError("always failed")
+from project_name import do_something, failed_func
 
 
 class Test_Name:
     @pytest.mark.parametrize(["value", "expected"], [["dummy test", "dummy test"]])
     def test_normal(self, value, expected):
-        assert dummy_func(value) == expected
+        assert do_something(value) == expected
 
     @pytest.mark.parametrize(["value", "expected"], [["nop", ValueError]])
     def test_exception(self, value, expected):
