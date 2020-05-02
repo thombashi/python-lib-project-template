4eb960c5bccb53e6d12f4d46a4e5009b5e98c2f5
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-05-02 15:27:21 +0900

    Modify a test case

--- test/test_template.py
+++ test/test_template.py
@@ -10,5 +10,7 @@ class Test_Name:
 
     @pytest.mark.parametrize(["value", "expected"], [["nop", ValueError]])
     def test_exception(self, value, expected):
-        with pytest.raises(expected):
+        with pytest.raises(expected) as e:
             failed_func(value)
+
+        assert "always failed" in str(e.value)
