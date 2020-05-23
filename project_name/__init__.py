20a7b1b1740079a6b254747330a0e31319e9e763
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-05-24 00:18:50 +0900

    Add docstrings

--- project_name/__init__.py
+++ project_name/__init__.py
@@ -3,10 +3,28 @@ from .logger import logger
 
 
 def do_something(value: str) -> str:
+    """[summary]
+
+    Args:
+        value (str): [description]
+
+    Returns:
+        str: [description]
+    """
+
     logger.debug("logging")
 
     return value
 
 
 def failed_func(value: str) -> None:
+    """[summary]
+
+    Args:
+        value (str): [description]
+
+    Raises:
+        ValueError: [description]
+    """
+
     raise ValueError("always failed")
