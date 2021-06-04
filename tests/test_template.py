5ea208fb537a174224dd20fadb73b1b09b94dd5d
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2021-06-04 21:40:20 +0900

    Modify format

--- tests/test_template.py
+++ tests/test_template.py
@@ -4,11 +4,21 @@ from project_name import do_something, failed_func
 
 
 class Test_Name:
-    @pytest.mark.parametrize(["value", "expected"], [["dummy test", "dummy test"]])
+    @pytest.mark.parametrize(
+        ["value", "expected"],
+        [
+            ["dummy test", "dummy test"],
+        ],
+    )
     def test_normal(self, value, expected):
         assert do_something(value) == expected
 
-    @pytest.mark.parametrize(["value", "expected"], [["nop", ValueError]])
+    @pytest.mark.parametrize(
+        ["value", "expected"],
+        [
+            ["nop", ValueError],
+        ],
+    )
     def test_exception(self, value, expected):
         with pytest.raises(expected) as e:
             failed_func(value)
