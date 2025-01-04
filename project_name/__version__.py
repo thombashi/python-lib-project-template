9022266711f856cfb5fbc72c068cd89fa6385794
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2025-01-04 09:42:13 +0900

    Fix version declaration syntax in __version__.py
    Signed-off-by: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>

--- project_name/__version__.py
+++ project_name/__version__.py
@@ -4,6 +4,6 @@ from typing import Final
 __author__: Final = "YOUR_NAME"
 __copyright__: Final = f"Copyright 2025, {__author__}"
 __license__: Final = "MIT License"
-__version__: Final = "0.0.1"
+__version__ = "0.0.1"
 __maintainer__: Final = __author__
 __email__: Final = "your.name@gmail.com"
